56ced8ac6e9af1bd5f0d59bc56d51c1b3bcd8ce9
cremaschi00 <74766288+cremaschi00@users.noreply.github.com>
2020-11-23 18:54:41 +0100

    AereoMotore aggiornato

--- Aeroporto/Aeroporto/CLASSES/AereoMotore.cs
+++ Aeroporto/Aeroporto/CLASSES/AereoMotore.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace Aeroporto.CLASSES
 {
-    class AereoMotore
+    class AereoMotore : Aeromobile,ICompare
     {
+        // Dichiaro la variabile Cavalli come intero
+        public int _Cavalli;
+        public AereoMotore(string sigla, int Cavalli) : base(sigla)
+        {
+            this._Cavalli = Cavalli;
+        }
+        // Implemento il metodo Superiore
+        public override bool Superiore()
+        {
+            ICompare(int _PotenzaSecondoAereo)
+            {
+                if (this._Cavalli > _PotenzaSecondoAereo)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        public int GetCavalli()
+        {
+            return _Cavalli;
+        }
     }
 }
