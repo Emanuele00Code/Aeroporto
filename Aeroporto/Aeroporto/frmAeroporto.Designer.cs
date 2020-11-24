95866460c2cfe3b16f00f7af7421ad2be6505ce9
Emanuele00Code <74766258+Emanuele00Code@users.noreply.github.com>
2020-11-24 09:50:58 +0100

    Aggiunta registrazione dati

--- Aeroporto/Aeroporto/frmAeroporto.Designer.cs
+++ Aeroporto/Aeroporto/frmAeroporto.Designer.cs
@@ -33,20 +33,14 @@ namespace Aeroporto
             this.label2 = new System.Windows.Forms.Label();
             this.txtSigla = new System.Windows.Forms.TextBox();
             this.updPotenza = new System.Windows.Forms.NumericUpDown();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radMotore2 = new System.Windows.Forms.RadioButton();
-            this.radAliante2 = new System.Windows.Forms.RadioButton();
-            this.updPotenza2 = new System.Windows.Forms.NumericUpDown();
-            this.txtSigla2 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.btnCompara = new System.Windows.Forms.Button();
+            this.btnRegistra = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
             this.radMotore = new System.Windows.Forms.RadioButton();
             this.radAliante = new System.Windows.Forms.RadioButton();
+            this.cmb1 = new System.Windows.Forms.ComboBox();
+            this.cmb2 = new System.Windows.Forms.ComboBox();
+            this.btnCompara = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.updPotenza)).BeginInit();
-            this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.updPotenza2)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -78,83 +72,24 @@ namespace Aeroporto
             // updPotenza
             // 
             this.updPotenza.Location = new System.Drawing.Point(222, 98);
+            this.updPotenza.Maximum = new decimal(new int[] {
+            9999,
+            0,
+            0,
+            0});
             this.updPotenza.Name = "updPotenza";
             this.updPotenza.Size = new System.Drawing.Size(120, 22);
             this.updPotenza.TabIndex = 3;
             // 
-            // groupBox1
+            // btnRegistra
             // 
-            this.groupBox1.Controls.Add(this.radMotore2);
-            this.groupBox1.Controls.Add(this.radAliante2);
-            this.groupBox1.Location = new System.Drawing.Point(428, 160);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
-            this.groupBox1.TabIndex = 4;
-            this.groupBox1.TabStop = false;
-            // 
-            // radMotore2
-            // 
-            this.radMotore2.AutoSize = true;
-            this.radMotore2.Location = new System.Drawing.Point(50, 62);
-            this.radMotore2.Name = "radMotore2";
-            this.radMotore2.Size = new System.Drawing.Size(73, 21);
-            this.radMotore2.TabIndex = 1;
-            this.radMotore2.TabStop = true;
-            this.radMotore2.Text = "Motore";
-            this.radMotore2.UseVisualStyleBackColor = true;
-            // 
-            // radAliante2
-            // 
-            this.radAliante2.AutoSize = true;
-            this.radAliante2.Location = new System.Drawing.Point(50, 35);
-            this.radAliante2.Name = "radAliante2";
-            this.radAliante2.Size = new System.Drawing.Size(72, 21);
-            this.radAliante2.TabIndex = 0;
-            this.radAliante2.TabStop = true;
-            this.radAliante2.Text = "Aliante";
-            this.radAliante2.UseVisualStyleBackColor = true;
-            // 
-            // updPotenza2
-            // 
-            this.updPotenza2.Location = new System.Drawing.Point(566, 98);
-            this.updPotenza2.Name = "updPotenza2";
-            this.updPotenza2.Size = new System.Drawing.Size(120, 22);
-            this.updPotenza2.TabIndex = 8;
-            // 
-            // txtSigla2
-            // 
-            this.txtSigla2.Location = new System.Drawing.Point(566, 49);
-            this.txtSigla2.Name = "txtSigla2";
-            this.txtSigla2.Size = new System.Drawing.Size(120, 22);
-            this.txtSigla2.TabIndex = 7;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(425, 103);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(114, 17);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Cavalli/Efficienza";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(425, 49);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(81, 17);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Sigla Aereo";
-            // 
-            // btnCompara
-            // 
-            this.btnCompara.Location = new System.Drawing.Point(362, 305);
-            this.btnCompara.Name = "btnCompara";
-            this.btnCompara.Size = new System.Drawing.Size(104, 51);
-            this.btnCompara.TabIndex = 9;
-            this.btnCompara.Text = "Compara";
-            this.btnCompara.UseVisualStyleBackColor = true;
-            this.btnCompara.Click += new System.EventHandler(this.btnCompara_Click_1);
+            this.btnRegistra.Location = new System.Drawing.Point(84, 312);
+            this.btnRegistra.Name = "btnRegistra";
+            this.btnRegistra.Size = new System.Drawing.Size(130, 51);
+            this.btnRegistra.TabIndex = 9;
+            this.btnRegistra.Text = "Aggiungi aereo";
+            this.btnRegistra.UseVisualStyleBackColor = true;
+            this.btnRegistra.Click += new System.EventHandler(this.btnCompara_Click_1);
             // 
             // groupBox2
             // 
@@ -188,18 +123,42 @@ namespace Aeroporto
             this.radAliante.Text = "Aliante";
             this.radAliante.UseVisualStyleBackColor = true;
             // 
+            // cmb1
+            // 
+            this.cmb1.FormattingEnabled = true;
+            this.cmb1.Location = new System.Drawing.Point(518, 46);
+            this.cmb1.Name = "cmb1";
+            this.cmb1.Size = new System.Drawing.Size(121, 24);
+            this.cmb1.TabIndex = 10;
+            // 
+            // cmb2
+            // 
+            this.cmb2.FormattingEnabled = true;
+            this.cmb2.Location = new System.Drawing.Point(645, 45);
+            this.cmb2.Name = "cmb2";
+            this.cmb2.Size = new System.Drawing.Size(121, 24);
+            this.cmb2.TabIndex = 11;
+            // 
+            // btnCompara
+            // 
+            this.btnCompara.Location = new System.Drawing.Point(598, 235);
+            this.btnCompara.Name = "btnCompara";
+            this.btnCompara.Size = new System.Drawing.Size(112, 61);
+            this.btnCompara.TabIndex = 12;
+            this.btnCompara.Text = "Compara";
+            this.btnCompara.UseVisualStyleBackColor = true;
+            this.btnCompara.Click += new System.EventHandler(this.btnCompara_Click);
+            // 
             // frmAeroporto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.btnCompara);
-            this.Controls.Add(this.updPotenza2);
-            this.Controls.Add(this.txtSigla2);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.cmb2);
+            this.Controls.Add(this.cmb1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.btnRegistra);
             this.Controls.Add(this.updPotenza);
             this.Controls.Add(this.txtSigla);
             this.Controls.Add(this.label2);
@@ -207,9 +166,6 @@ namespace Aeroporto
             this.Name = "frmAeroporto";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.updPotenza)).EndInit();
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.updPotenza2)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
@@ -223,17 +179,13 @@ namespace Aeroporto
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtSigla;
         private System.Windows.Forms.NumericUpDown updPotenza;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radMotore2;
-        private System.Windows.Forms.RadioButton radAliante2;
-        private System.Windows.Forms.NumericUpDown updPotenza2;
-        private System.Windows.Forms.TextBox txtSigla2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button btnCompara;
+        private System.Windows.Forms.Button btnRegistra;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.RadioButton radMotore;
         private System.Windows.Forms.RadioButton radAliante;
+        private System.Windows.Forms.ComboBox cmb1;
+        private System.Windows.Forms.ComboBox cmb2;
+        private System.Windows.Forms.Button btnCompara;
     }
 }
 
--- Aeroporto/Aeroporto/frmAeroporto.cs
+++ Aeroporto/Aeroporto/frmAeroporto.cs
@@ -66,6 +66,26 @@ namespace Aeroporto
         {
             if (radAliante.Checked)
             {
+                string dato = "";
+                dato += "AL-";
+                dato += txtSigla.Text;
+                dato += ";";
+                dato += (int)updPotenza.Value;
+                cmb1.Items.Add(dato);
+                cmb2.Items.Add(dato);
+            }
+            else
+            {
+                string dato = "";
+                dato += "AM-";
+                dato += txtSigla.Text;
+                dato += ";";
+                dato += (int)updPotenza.Value;
+                cmb1.Items.Add(dato);
+                cmb2.Items.Add(dato);
+            }
+            /*if (radAliante.Checked)
+            {
                 Aliante aliante1 = new Aliante(txtSigla.Text, (int)updPotenza.Value);
                 if (radAliante2.Checked)
                 {
@@ -101,9 +121,61 @@ namespace Aeroporto
                 else
                     MessageBox.Show("Hai selezionato due aerei non comparabili!");
 
+            }*/
+
+
+        }
+
+        private void btnCompara_Click(object sender, EventArgs e)
+        {
+            int posizionetratto1 = cmb1.SelectedItem.ToString().IndexOf("-");
+            int posizionepv1 = cmb1.SelectedItem.ToString().IndexOf(";");
+            string tipo1 = cmb1.SelectedItem.ToString().Substring(0, posizionetratto1);
+            string sigla1 = cmb1.SelectedItem.ToString().Substring(posizionetratto1 + 1, posizionepv1 - posizionetratto1 - 1);
+            string potenza1 = cmb1.SelectedItem.ToString().Substring(posizionepv1 + 1);
+            int power1 = Convert.ToInt32(potenza1); ;
+
+
+
+            int posizionetratto2 = cmb2.SelectedItem.ToString().IndexOf("-");
+            int posizionepv2 = cmb2.SelectedItem.ToString().IndexOf(";");
+            string tipo2 = cmb2.SelectedItem.ToString().Substring(0, posizionetratto2);
+            string sigla2 = cmb2.SelectedItem.ToString().Substring(posizionetratto2 + 1, posizionepv2 - posizionetratto2 - 1);
+            string potenza2 = cmb2.SelectedItem.ToString().Substring(posizionepv2 + 1);
+            int power2 = Convert.ToInt32(potenza2); ;
+
+            if (tipo2 == tipo1)
+            {
+                if (tipo1 == "AL")
+                {
+                    Aliante aliante1 = new Aliante(sigla1, power1);
+                    Aliante aliante2 = new Aliante(sigla2, power2);
+                    if (aliante1.Superiore(aliante2.GetEfficienza()))
+                    {
+                        MessageBox.Show(aliante1.GetSigla() + " ha più potenza del " + aliante2.GetSigla());
+                    }
+                    else
+                    {
+                        MessageBox.Show(aliante1.GetSigla() + " NON ha più potenza del " + aliante2.GetSigla());
+                    }
+                }
+                else
+                {
+                    AereoMotore aereomotore1 = new AereoMotore(sigla1, power1);
+                    AereoMotore aereomotore2 = new AereoMotore(sigla2, power2);
+                    if (aereomotore1.Superiore(aereomotore2.GetCavalli()))
+                    {
+                        MessageBox.Show(aereomotore1.GetSigla() + " ha più potenza del " + aereomotore2.GetSigla());
+                    }
+                    else
+                    {
+                        MessageBox.Show(aereomotore1.GetSigla() + " NON ha più potenza del " + aereomotore2.GetSigla());
+                    }
+                }
+                
             }
-
-
+            else
+                MessageBox.Show("Aerei non compatibili");
         }
     }
 }
